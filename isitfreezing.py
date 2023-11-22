930b954e869860d8649cecd469d38ef4f9205ee9
nbrinton <nab.natethegreat@gmail.com>
2023-11-22 15:08:57 -0800

    - Added open meteo's python code and updated `.gitignore` with line for the new caching.

--- isitfreezing.py
+++ isitfreezing.py
@@ -1,3 +1,8 @@
+import openmeteo_requests
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
 import mailtrap as mt
 import os
 import requests
@@ -25,8 +30,7 @@ def send_email(freezing_days):
     client = mt.MailtrapClient(token=os.environ['MAILTRAP_SMTP_PASSWORD'])
     client.send(mail)
 
-
-if __name__ == '__main__':
+def my_way():
     payload = {
         'forecast_days': 16,
         'latitude': 43.582030,
@@ -62,3 +66,54 @@ if __name__ == '__main__':
 
     if len(freezing_days) > 0:
         send_email(freezing_days=freezing_days)
+
+
+def meteo_way():
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": 43.58203,
+        "longitude": -116.19821,
+        "daily": ["temperature_2m_min", "apparent_temperature_min"],
+        "temperature_unit": "fahrenheit",
+        "wind_speed_unit": "mph",
+        "precipitation_unit": "inch",
+        "timezone": "America/Denver",
+        "forecast_days": 16
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+    # Process daily data. The order of variables needs to be the same as requested.
+    daily = response.Daily()
+    daily_temperature_2m_min = daily.Variables(0).ValuesAsNumpy()
+    daily_apparent_temperature_min = daily.Variables(1).ValuesAsNumpy()
+
+    daily_data = {"date": pd.date_range(
+        start=pd.to_datetime(daily.Time(), unit="s"),
+        end=pd.to_datetime(daily.TimeEnd(), unit="s"),
+        freq=pd.Timedelta(seconds=daily.Interval()),
+        inclusive="left"
+    )}
+    daily_data["temperature_2m_min"] = daily_temperature_2m_min
+    daily_data["apparent_temperature_min"] = daily_apparent_temperature_min
+
+    daily_dataframe = pd.DataFrame(data=daily_data)
+    print(daily_dataframe)
+
+
+if __name__ == '__main__':
+    # my_way()
+    meteo_way()
